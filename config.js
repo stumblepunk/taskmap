bef55e729b402709f0f0c418ebfb7eac855d0bb0
stumblepunk <davidjdempsey@gmail.com>
2026-04-12 15:47:27 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// ============================================================
+// TASK MAP CONFIG
+// Paste your Apps Script Web App URL below after deploying
+// ============================================================
+
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyCR_QpDdlnlLdksYPyHodCHyBUohU40BrcTBsR909PuFKpEcmRKmy5fBSOyuAQVYbjTg/exec";
